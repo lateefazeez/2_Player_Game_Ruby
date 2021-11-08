ebbd4c9125b8fe2fa60ebfbec782a1e40576e5eb
Lateef Azeez <codelate18@gmail.com>
2021-11-08 18:28:34 +0000

    Add the planning file

---
+++ Planning.rb
@@ -0,0 +1,18 @@
+# Nouns => Player, Question, Game
+
+# Player Class => defines the rules for creating players, including player name and lives
+
+# Question Class => defines and output the questions for player turns and also check for question correctness
+
+# Game class => runs the game, update the rounds, determine player turns and end the game
+
+# Public Methods
+
+# Player => update_life
+# Question => print_question, check_answer
+# Game => play_game, update_rounds, switch_turn, end_game
+
+
+# Game Loop => Contained in the Game class
+# Current Player => The Game class
+# Input/Output => The Question Class
